dd30daf7533b84ec5c7302ac2355a1daa2bc51d2
Lucas-mg <lucas.gordillo.3591@gmail.com>
2025-09-27 09:56:50 -0300

    Create reporte.js

---
+++ Ejercicios/reporte.js
@@ -0,0 +1,21 @@
+async function listarProductosMayorAlPromedio() {
+    const db = await conectar();
+    const productos = db.collection("productos");
+    const resultadoPromedio = await productos.aggregate([
+        { $group: { _id: null, promedio: { $avg: "$precio" } } }
+    ]).toArray();
+    const promedio = resultadoPromedio[0].promedio;
+    console.log(`El promedio de precios es: $${promedio.toFixed(2)}`);
+    const productosMayores = await productos.find({ precio: { $gt: promedio } }).toArray();
+    console.log("🔍 Productos con precio mayor al promedio:");
+    productosMayores.forEach(producto => {
+        console.log(`   - ${producto.nombre}: $${producto.precio.toFixed(2)}`);
+    });
+    return productosMayores;
+}
+
+(async () => {
+    await listarProductosMayorAlPromedio();
+    console.log("\n" + "=".repeat(50));
+    
+})();
